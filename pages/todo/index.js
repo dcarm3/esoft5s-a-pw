4638894092447604b064ddc4769c0054ec3b994c
Daniel <95975734+dcarm3@users.noreply.github.com>
2024-05-24 00:44:04 +0000

    botão editar todo

--- pages/todo/index.js
+++ pages/todo/index.js
@@ -1,28 +1,60 @@
-function enviar() {
-    const formulario = document.getElementById("formTarefas");
-    let titulo = document.getElementById("titulo").value;
-    let descricao = document.getElementById("descricao").value;
+const taskKey = '@tasks'
 
-    let listaTarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
-    listaTarefas.push({ titulo: titulo, descricao: descricao });
-    localStorage.setItem("tarefas", JSON.stringify(listaTarefas));
-    document.getElementById("titulo").value = "";
-    document.getElementById("descricao").value = "";
+function addTask(event) {
+    event.preventDefault()
+    const taskId = new Date().getTime()
+    const taskList = document.querySelector('#taskList')
+
+    const form = document.querySelector('#taskForm')
+    const formData = new FormData(form)
+
+    const taskTitle = formData.get('title')
+    const taskDescription = formData.get('description')
+
+    const li = document.createElement('li')
+
+    li.id = taskId
+    li.innerHTML = `
+      <h2>${taskTitle}</h2>
+      <p>${taskDescription}</p>
+      <button class="edit-button" title="Editar tarefa">✏️</button>
+    `
+
+    taskList.appendChild(li)
+
+    const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
+    tasks.push({ id: taskId, title: taskTitle, description: taskDescription })
+    localStorage.setItem(taskKey, JSON.stringify(tasks))
+
+    form.reset()
 }
 
-function carregarTarefas() {
-    const lista = document.querySelector(".listaTarefas");
-    lista.innerHTML = "";
-    let listaTarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+window.addEventListener('DOMContentLoaded', () => {
+    const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
+    const taskList = document.querySelector('#taskList')
+    taskList.innerHTML = tasks.map((task) => `
+        <li id="${task.id}">
+            <h2>${task.title}</h2>
+            <p>${task.description}</p>
+            <button class="edit-button" title="Editar tarefa">✏️</button>
+        </li>`).join('')
 
-    listaTarefas.forEach(function (tarefa) {
-        const item = document.createElement("li");
-        item.innerHTML = `Tarefa: ${tarefa.titulo} <br> Descrição: ${tarefa.descricao}`;
-        lista.appendChild(item);
-    });
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    carregarTarefas();
+    document.querySelectorAll('.edit-button').forEach(button => {
+        button.addEventListener('click', openEditDialog)
+    })
 })
+
+function openEditDialog(event) {
+    const li = event.target.closest('li')
+    const taskId = li.id
+
+    const taskTitle = li.querySelector('h2').textContent
+    const taskDescription = li.querySelector('p').textContent
+    const dialog = document.querySelector('#editDialog')
+    dialog.querySelector('#editTitle').value = taskTitle
+    dialog.querySelector('#editDescription').value = taskDescription
+    dialog.showModal()
+    document.querySelector('#cancelb').addEventListener('click', () => {
+        dialog.close()
+    })
+}
